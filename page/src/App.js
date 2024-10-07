2571c52b25564592a5308ba6ab0b98f610dbcc8d
ayesha19297 <ayesha19297@gmail.com>
2024-10-07 14:46:47 +0530

    initial commit

---
+++ page/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to our landing page</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ page/src/Components/Navbar/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+    <div className="nav1">
+        <img src="Logo.png" alt="Logo"></img>
+    </div>
+    <div className="nav2">
+      <div>
+        <strong>Shop</strong>
+      </div>
+      <div>
+        <strong>Skills</strong>
+      </div>
+      <div>
+        <strong>Stories</strong>
+      </div>
+      <div>
+        <strong>About</strong>
+      </div>
+      <div>
+        <strong>Contact us</strong>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Navbar;
